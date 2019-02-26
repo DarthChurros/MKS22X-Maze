bdf5dddae60f335950c29599f1afc52307449cb2
DarthChurros <tnarayan00@stuy.edu>
2019-02-26 11:44:16 -0500

    filled array

--- Maze.java
+++ Maze.java
@@ -5,15 +5,34 @@ import java.util.Scanner;
 public class Maze {
   public static void main(String[] args) {
     Scanner in;
+    int rows = 0;
+    int cols = 0;
     try {
       in = new Scanner(new File("Maze1.txt"));
       while (in.hasNextLine()) {
-        System.out.println(in.nextLine());
+        String line = in.nextLine();
+        System.out.println(line);
+        rows++;
+        cols = line.length() - 1;
       }
     } catch (FileNotFoundException e) {
       System.out.println("file not found!");
       System.exit(1);
     }
-
+    char[][] board = new char[rows][cols];
+    try {
+      in = new Scanner(new File("Maze1.txt"));
+      int row = 0;
+      while (in.hasNextLine()) {
+        String line = in.nextLine();
+        for (int i = 0; i < cols; i++) {
+          board[row][i] = line.charAt(i);
+        }
+        row++;
+      }
+    } catch (FileNotFoundException e) {
+      System.out.println("File not found!");
+      System.exit(1);
+    }
   }
 }
