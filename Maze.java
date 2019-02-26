02a0baf57a3249f1ee47f8b2a445acb0c175b584
DarthChurros <tnarayan00@stuy.edu>
2019-02-26 11:46:33 -0500

    filled array

--- Maze.java
+++ Maze.java
@@ -34,5 +34,13 @@ public class Maze {
       System.out.println("File not found!");
       System.exit(1);
     }
+
+    System.out.println("PRINTING FROM ARRAY\n\n");
+    for (int i = 0; i < board.length; i++) {
+      for (int j = 0; j < board[i].length; j++) {
+        System.out.print(board[i][j]);
+      }
+      System.out.print('\n');
+    }
   }
 }
