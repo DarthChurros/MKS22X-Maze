348b95d861bd0bc612cdb6a48e7ee0820f9148c1
DarthChurros <tnarayan00@stuy.edu>
2019-02-27 11:04:42 -0500

    wrote maze constructor from main

--- Maze.java
+++ Maze.java
@@ -3,6 +3,45 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Maze {
+
+  private char[][] maze;
+  private boolean animate;
+
+  public Maze(String filename) {
+    Scanner in;
+    int rows = 0;
+    int cols = 0;
+    try {
+      in = new Scanner(new File(filename));
+      while (in.hasNextLine()) {
+        String line = in.nextLine();
+        rows++;
+        cols = line.length();
+      }
+    } catch (FileNotFoundException e) {
+      System.out.println("File not found!");
+      System.exit(1);
+    }
+    maze = new char[rows][cols];
+    try {
+      in = new Scanner(new File(filename));
+      int row = 0;
+      while (in.hasNextLine()) {
+        String line = in.nextLine();
+        for (int i = 0; i < cols; i++) {
+          maze[row][i] = line.charAt(i);
+        }
+        row++;
+      }
+    } catch (FileNotFoundException e) {
+      System.out.println("File not found!");
+      System.exit(1);
+    }
+    animate = false;
+  }
+
+
+
   public static void main(String[] args) {
     Scanner in;
     int rows = 0;
