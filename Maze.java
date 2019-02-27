aa18230eb525ca6f0955353295f065adcb3aa9ae
DarthChurros <tnarayan00@stuy.edu>
2019-02-27 11:15:41 -0500

    wrote toString, test main

--- Maze.java
+++ Maze.java
@@ -37,46 +37,27 @@ public class Maze {
     animate = false;
   }
 
+  public String toString() {
+    String ans = "";
+    for (int i = 0; i < maze.length; i++) {
+      for (int j = 0; j < maze[i].length; j++) {
+        ans += maze[i][j];
+      }
+      ans += '\n';
+    }
+    return ans;
+  }
+
 
 
   public static void main(String[] args) {
-    Scanner in;
-    int rows = 0;
-    int cols = 0;
+    Maze test;
     try {
-      in = new Scanner(new File("Maze1.txt"));
-      while (in.hasNextLine()) {
-        String line = in.nextLine();
-        System.out.println(line);
-        rows++;
-        cols = line.length();
-      }
-    } catch (FileNotFoundException e) {
-      System.out.println("file not found!");
-      System.exit(1);
-    }
-    char[][] board = new char[rows][cols];
-    try {
-      in = new Scanner(new File("Maze1.txt"));
-      int row = 0;
-      while (in.hasNextLine()) {
-        String line = in.nextLine();
-        for (int i = 0; i < cols; i++) {
-          board[row][i] = line.charAt(i);
-        }
-        row++;
-      }
+      test = new Maze("Maze1.txt");
+      System.out.println(test);
     } catch (FileNotFoundException e) {
       System.out.println("File not found!");
       System.exit(1);
-    }
-
-    System.out.println("PRINTING FROM ARRAY\n\n");
-    for (int i = 0; i < board.length; i++) {
-      for (int j = 0; j < board[i].length; j++) {
-        System.out.print(board[i][j]);
-      }
-      System.out.print('\n');
     }
   }
 }
